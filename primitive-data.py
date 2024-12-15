faa8301cf531433e092cff35cec9f43dcec963bd
marziamostafa <marziamostafa@gmail.com>
2024-12-15 20:13:58 +0600

    first commit

---
+++ primitive-data.py
@@ -0,0 +1,10 @@
+i_value = 85 #integer
+f_value = 5.264 #float
+s_value = "Marzia Mostafa" #string
+b_value = True #boolean
+
+
+print(f"Integer value: {i_value} and type: {type(i_value)}")
+print(f"Floatig value: {f_value} and type: {type(f_value)}")
+print(f"String value: {s_value} and type: {type(s_value)}")
+print(f"Boolean value: {b_value} and type: {type(b_value)}")
