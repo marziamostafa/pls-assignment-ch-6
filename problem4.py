6b6b457f3bf92128ef51554feb77e265f75da948
marziamostafa <marziamostafa@gmail.com>
2024-12-16 12:00:55 +0600

    code for question 4

---
+++ problem4.py
@@ -0,0 +1,11 @@
+
+array = [9,6,25,87,90]
+
+
+print(f"First Element: {array[0]}")  # Access by index
+print(f"Last Element: {array[4]}")
+print(f"legth of array is: {len(array)}")
+
+
+array.append(6)
+print(f"Updated Array: {array}")
